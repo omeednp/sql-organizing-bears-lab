5b0576ea210486e92c2919eab5c9043ce486d0f4
omeednp <omeed.naraghipour@gmail.com>
2021-01-21 14:24:57 -0800

    done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (1, "Mr. Chocolate", 5, "M", "Brown", "Happy", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (2, "Rowdy", 6, "M", "Yellow", "Rowdy", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (3, "Tabitha", 7, "F", "Orange", "Calm", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (4, "Sergeant Brown", 8, "M", "Brown", "Aggressive", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (5, "Melissa", 3, "F", "Black", "Sad", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (6, "Grinch", 12, "M", "Green", "Mean", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (7, "Wendy", 2, "F", "Pink", "Bubbly", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (8, NULL, 1, "M", "White", "Hungry", 0)
